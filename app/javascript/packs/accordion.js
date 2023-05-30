ff091cedffdf82cd1b5d2242aa4766f73321453a
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-05-30 21:46:46 +0100

    New edits

--- app/javascript/packs/accordion.js
+++ app/javascript/packs/accordion.js
@@ -2,6 +2,11 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 
 accordionItemHeaders.forEach(accordionItemHeader => {
   accordionItemHeader.addEventListener("click", event => {
+    // const currentlyActiveAccordionHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionHeader && currentlyActiveAccordionHeader!==accordionItemHeader){
+    //   currentlyActiveAccordionHeader.classList.toggle("active");
+    //   currentlyActiveAccordionHeader.nextElementSibling.style.maxHeight = 0;
+    // }
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if(accordionItemHeader.classList.contains("active")) {
