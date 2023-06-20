3139ae5b0c01b0b14bd520e91a492d5ab8929cb9
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-06-20 03:04:27 +0100

    Implement flash messages display and clean up view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,14 @@ module ApplicationHelper
       ]
     end
   end
+
+  def alert_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info',
+      congratulations: 'alert-success'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
