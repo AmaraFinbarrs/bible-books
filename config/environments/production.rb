ef6fb6a4035561386c7f05059a8d672f8553fcd5
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-06-20 03:04:02 +0100

    Other devise configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'journey.include-uk.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'bible-books.onrender.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
