b2956bd648c0842219fb5ddec5b8aef9105a65b8
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-06-22 22:45:20 +0100

    allow db/config.rb file

---
+++ db/config.rb
@@ -0,0 +1,10 @@
+# db/config.rb
+
+# All the constants for the seed data are found here
+module Config
+  TOTAL_MEMBER_COUNT = 4
+  TOTAL_ADMIN_COUNT = 4
+  VERSE_NOTE_COUNT = 50
+  CHAPTER_NOTE_COUNT = 50
+  BOOK_NOTE_COUNT = 50
+end
