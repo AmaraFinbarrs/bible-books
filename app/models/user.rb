cb985b7d836d783996faa9d5d64561b038b19f70
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-06-22 22:35:51 +0100

    ensure all new users are registered as members.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   has_many :notes
 
   belongs_to :portal
+
+  before_validation :assign_portal, on: :create
+
+  private
+
+  def assign_portal
+    self.portal = Portal.find_by(name: 'member') if portal.blank?
+  end
 end
