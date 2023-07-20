70776eda30f9b2eda3e38fc68af7f9e4c90895b6
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-07-20 01:36:15 +0100

    Adapt webpacker setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
-const { environment } = require('@rails/webpacker')
+// const { environment } = require('@rails/webpacker')
+// const webpack = require("webpack")
+
+const { resolve } = require('path');
+const { config, environment, Environment } = require('@rails/webpacker');
+const WebpackerPwa = require('webpacker-pwa');
 const webpack = require("webpack")
+new WebpackerPwa(config, environment);
+module.exports = environment;
 
 // Adds `var jQuery = require('jquery') to legacy jQuery plugins
 environment.plugins.append(
